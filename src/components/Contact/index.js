d8b08ffa16f6998f9bb54f503f07c7a0afd67c13
Laxmikant <laxmikantsarswat007@gmail.com>
2023-10-27 23:36:31 +0530

    Details Updated

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -130,7 +130,7 @@ const Contact = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    emailjs.sendForm('service_tox7kqs', 'template_nv7k7mj', form.current, 'SybVGsYS52j2TfLbi')
+    emailjs.sendForm('service_zqma4yh', 'template_4rtq1rn', form.current, 'he_f4egCj4GbBaUu9')
       .then((result) => {
         setOpen(true);
         form.current.reset();
